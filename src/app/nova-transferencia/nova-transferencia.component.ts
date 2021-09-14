f2b51c1176609ad99cde120ea8c0f8077bcbc68d
estevaoreis25 <estevaoreis1587@gmail.com>
2021-09-14 11:25:26 -0300

    Criação da do componente de registrar nova transferencia. Exibição dos dados na página principal

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,26 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector:'app-nova-transferencia',
+  templateUrl:'./nova-transferencia.component.html',
+  styleUrls:['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent{
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir(){
+    console.log(`Valor: ${this.valor} \nDestino: ${this.destino}`);
+    const valorEmitir = {valor: this.valor, destino:this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = undefined;
+    this.destino = undefined;
+  }
+}
